62975b3a5933232c31d65eb9b407ff7211d1fc7f
maryaleb8 <90481967+maryaleb8@users.noreply.github.com>
2021-11-23 17:40:33 +0300

    Add files via upload

---
+++ 02.2_write.c
@@ -0,0 +1,33 @@
+/*без цикла то же самое*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    if(argc != 3) {
+        fprintf(stderr, "Usage: %s filename string\n", argv[0]);
+        return 1;
+    }
+
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if(fd < 0) {
+        perror("Failure in opening");
+        return 2;
+    }
+    if(dprintf(fd, "%s", argv[2]) < 0) {
+        perror("Failure in writing");
+        close(fd);
+        return 3;
+    }
+    if(close(fd) < 0) {
+        perror("Failure in close");
+        return 4;
+    }
+
+    return 0;
+}
