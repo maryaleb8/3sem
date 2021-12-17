82fde6020bf0b2e68a0c2e7c5506911deaf91859
Marya Lebedeva <lebedeva.mk@phystech.edu>
2021-12-17 23:51:04 +0300

    updated after talk

--- 03.2_copy.c
+++ 03.2_copy.c
@@ -10,40 +10,43 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
+    int result = 0;
     if(argc != 3) {
         fprintf(stderr, "Usage: %s source destination\n", argv[0]);
         return 1;
     }
-
+    struct stat sb;
+    if (lstat(argv[1], &sb) < 0){
+        perror("lstat");
+        return 2;
+    }
+    if(!S_ISREG(sb.st_mode)) { //слишком страшная строка, макросы if(!S_ISREG(sb.st_mode))
+        perror("Not a regular file");
+        return 3;
+    }
     int fd1 = open(argv[1], O_RDONLY);
     if(fd1 < 0) {
         perror("Failure in opening first file");
-        return 2;
+        result = 4;
     }
     int fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if(fd2 < 0) {
         perror("Failure in opening second file");
-        return 3;
-    }
-
-    struct stat sb;
-    lstat(argv[1], &sb);
-    if (((sb.st_mode) & (S_IFMT)) != S_IFREG) {
-        perror("Not a regular file");
-        return 4;
+        result = 5;
     }
 
     char text[sb.st_size];
     ssize_t countr, countw;
     off_t offset_r = 0, offset_w = 0;
 
-    while ((countr = pread(fd1, buf, sizeof(buf), offset_r)) > 0) {
+    while ((countr = pread(fd1, text, sizeof(text), offset_r)) > 0 && result == 0) {
       offset_r += countr;
       while (countr) {
-        countw = pwrite(fd2, text, countr, offset_w);
+        countw = pwrite(fd2, text, (size_t)countr, offset_w);
         if (countw == -1) {
           perror("Failure in write");
-          return 5;
+          result = 6;
+          break;
         }
         countr -= countw;
         offset_w += countw;
@@ -51,17 +54,17 @@ int main(int argc, char* argv[]) {
     }
     if (countr == -1) {
       perror("Failure in read");
-      return 6;
+      result = 7;
     }
 
     if(close(fd1) < 0) {
         perror("Failure in close first");
-        return 7;
+        return 8;
     }
     if(close(fd2) < 0) {
         perror("Failure in close second");
-        return 8;
+        return 9;
     }
 
-    return 0;
+    return result;
 }
