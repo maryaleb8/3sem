3125d9fffc44a0458f31e4be58b4424aacf41cc4
Marya Lebedeva <lebedeva.mk@phystech.edu>
2021-12-17 23:45:49 +0300

    updated after talk

--- 03.1_copy.c
+++ 03.1_copy.c
@@ -9,51 +9,49 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
+    int result = 0;
     if(argc != 3) {
         fprintf(stderr, "Usage: %s source destination\n", argv[0]);
         return 1;
     }
-
+    struct stat sb;
+    if (lstat(argv[1], &sb) < 0){
+        perror("lstat");
+        return 2;
+    }
+    if(!S_ISREG(sb.st_mode)) { //слишком страшная строка, макросы if(!S_ISREG(sb.st_mode))
+        perror("Not a regular file");
+        return 3;
+    }
     int fd1 = open(argv[1], O_RDONLY);
     if(fd1 < 0) {
         perror("Failure in opening first file");
-        return 2;
+        result = 4;
     }
     int fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if(fd2 < 0) {
         perror("Failure in opening second file");
-        //close для входлного открывания
-        return 3;
-    }
-
-    struct stat sb;
-    //if (lstat((argv[1], &sb
-    lstat(argv[1], &sb);//обработать ошибку, сначала стат, потом открываем, нам нужно только то что открыть нужно
-    if ((sb.st_mode & S_IFMT) != S_IFREG) { //слишком страшная строка, макросы if(!S_ISREG(sb.st_mode))
-        perror("Not a regular file");
-        return 4;
+        result = 5;
     }
 
     char text [sb.st_size];//слишком мало памяти, нужно авести буфер размера, нужно сравнить числа которые мы считали и изначальный размер файла
-    if (read(fd1, &text, sb.st_size) == -1) {//write all
+    if (read(fd1, &text, (size_t)sb.st_size) == -1) {
         perror("Failed to read first file");
-        return 5;
+        result = 6;
     }
 
     if(dprintf(fd2, "%s", text) < 0) {
         perror("Failure in writing");
-        close(fd1);
-        close(fd2);
-        return 6;
+        result = 7;
     }
     if(close(fd1) < 0) {
         perror("Failure in close first");
-        return 7;
+        return 8;
     }
     if(close(fd2) < 0) {
         perror("Failure in close second");
-        return 8;
+        return 9;
     }
 
-    return 0;
+    return result;
 }
