953da85ab100d649924a687bc7c9b0713fa24617
Marya Lebedeva <lebedeva.mk@phystech.edu>
2021-12-21 01:13:50 +0300

    changed to new version 64

--- 06.3_getdents.c
+++ 06.3_getdents.c
@@ -1,4 +1,4 @@
-//как я люблю код из мана 
+//как я люблю код из мана
 #define _GNU_SOURCE
 #include <dirent.h>     /* Defines DT_* constants */
 #include <fcntl.h>
@@ -11,10 +11,11 @@
 #define handle_error(msg) \
     do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-struct linux_dirent {
-    long           d_ino;
-    off_t          d_off;
+struct linux_dirent64 {
+    ino64_t           d_ino;
+    off64_t          d_off;
     unsigned short d_reclen;
+    unsigned char  d_type;
     char           d_name[];
 };
 
@@ -22,11 +23,15 @@ struct linux_dirent {
 #define BUF_SIZE 1024
 
 int main(int argc, char *argv[]){
+    if (argc > 2) {
+        printf("Usage: %s [directory]\n", argv[0]);
+        return 1;
+    }
+
    int fd, nread;
    char buf[BUF_SIZE];
-   struct linux_dirent *d;
+   struct linux_dirent64 *d;
    int bpos;
-   char d_type;
 
    fd = open(argc > 1 ? argv[1] : ".", O_RDONLY | O_DIRECTORY);
 
@@ -34,7 +39,7 @@ int main(int argc, char *argv[]){
        handle_error("open");
 
    for ( ; ; ) {
-       nread = (int)syscall(SYS_getdents, fd, buf, BUF_SIZE);
+       nread = (int)syscall(SYS_getdents64, fd, buf, BUF_SIZE);
        if (nread == -1)
            handle_error("getdents");
 
@@ -44,16 +49,15 @@ int main(int argc, char *argv[]){
        printf("--------------- nread=%d ---------------\n", nread);
        printf("inode#    file type  d_reclen  d_off   d_name\n");
        for (bpos = 0; bpos < nread;) {
-           d = (struct linux_dirent *) (buf + bpos);
-           printf("%8ld  ", d->d_ino);
-           d_type = *(buf + bpos + d->d_reclen - 1);
-           printf("%-10s ", (d_type == DT_REG) ?  "regular" :
-                            (d_type == DT_DIR) ?  "directory" :
-                            (d_type == DT_FIFO) ? "FIFO" :
-                            (d_type == DT_SOCK) ? "socket" :
-                            (d_type == DT_LNK) ?  "symlink" :
-                            (d_type == DT_BLK) ?  "block dev" :
-                            (d_type == DT_CHR) ?  "char dev" : "???");
+           d = (struct linux_dirent64 *) (buf + bpos);
+           printf("%8lu  ", d->d_ino);
+           printf("%-10s ", (d->d_type == DT_REG) ?  "regular" :
+                            (d->d_type == DT_DIR) ?  "directory" :
+                            (d->d_type == DT_FIFO) ? "FIFO" :
+                            (d->d_type == DT_SOCK) ? "socket" :
+                            (d->d_type == DT_LNK) ?  "symlink" :
+                            (d->d_type == DT_BLK) ?  "block dev" :
+                            (d->d_type == DT_CHR) ?  "char dev" : "???");
            printf("%4d %10lld  %s\n", d->d_reclen,
                    (long long) d->d_off, d->d_name);
            bpos += d->d_reclen;
